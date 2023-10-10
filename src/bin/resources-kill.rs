aa3353ccd1e8fa6f71e00fc96de4b76fce039267
nokyan <nokyan@tuta.io>
2023-10-10 19:48:13 +0200

    Remove nightly stuff from resources-kill

--- src/bin/resources-kill.rs
+++ src/bin/resources-kill.rs
@@ -1,26 +1,26 @@
-#![feature(let_chains)]
-
 use std::env;
 
 use nix::{sys::signal, unistd::Pid};
 
 fn main() {
-    if let Some(arg) = env::args().nth(1) && let Some(pid) = env::args().nth(2).and_then(|s| s.trim().parse().ok()) {
-        let signal = match arg.as_str() {
-            "STOP" => signal::Signal::SIGSTOP,
-            "CONT" => signal::Signal::SIGCONT,
-            "TERM" => signal::Signal::SIGTERM,
-            "KILL" => signal::Signal::SIGKILL,
-            _ => std::process::exit(254),
-        };
-        let result = signal::kill(Pid::from_raw(pid), Some(signal));
-        if let Err(err) = result {
-            match err {
-                nix::errno::Errno::UnknownErrno => std::process::exit(253),
-                _ => std::process::exit(err as i32),
+    if let Some(arg) = env::args().nth(1) {
+        if let Some(pid) = env::args().nth(2).and_then(|s| s.trim().parse().ok()) {
+            let signal = match arg.as_str() {
+                "STOP" => signal::Signal::SIGSTOP,
+                "CONT" => signal::Signal::SIGCONT,
+                "TERM" => signal::Signal::SIGTERM,
+                "KILL" => signal::Signal::SIGKILL,
+                _ => std::process::exit(254),
             };
+            let result = signal::kill(Pid::from_raw(pid), Some(signal));
+            if let Err(err) = result {
+                match err {
+                    nix::errno::Errno::UnknownErrno => std::process::exit(253),
+                    _ => std::process::exit(err as i32),
+                };
+            }
+            std::process::exit(0);
         }
-        std::process::exit(0);
-    };
+    }
     std::process::exit(255);
 }
