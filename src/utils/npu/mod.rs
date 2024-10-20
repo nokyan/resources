7387cd5edba68f38c4624e290a44468b070a7caf
nokyan <nokyan@tuta.io>
2024-10-20 11:17:44 +0200

    Introduce temperature graphs for NPUs as well

--- src/utils/npu/mod.rs
+++ src/utils/npu/mod.rs
@@ -27,13 +27,13 @@ pub struct NpuData {
 
     pub usage_fraction: Option<f64>,
 
-    pub total_vram: Option<usize>,
-    pub used_vram: Option<usize>,
+    pub total_memory: Option<usize>,
+    pub used_memory: Option<usize>,
 
     pub clock_speed: Option<f64>,
     pub vram_speed: Option<f64>,
 
-    pub temp: Option<f64>,
+    pub temperature: Option<f64>,
 
     pub power_usage: Option<f64>,
     pub power_cap: Option<f64>,
@@ -46,13 +46,13 @@ impl NpuData {
 
         let usage_fraction = npu.usage().ok();
 
-        let total_vram = npu.total_vram().ok();
-        let used_vram = npu.used_vram().ok();
+        let total_memory = npu.total_vram().ok();
+        let used_memory = npu.used_vram().ok();
 
         let clock_speed = npu.core_frequency().ok();
-        let vram_speed = npu.vram_frequency().ok();
+        let vram_speed = npu.memory_frequency().ok();
 
-        let temp = npu.temperature().ok();
+        let temperature = npu.temperature().ok();
 
         let power_usage = npu.power_usage().ok();
         let power_cap = npu.power_cap().ok();
@@ -61,11 +61,11 @@ impl NpuData {
         Self {
             pci_slot,
             usage_fraction,
-            total_vram,
-            used_vram,
+            total_memory,
+            used_memory,
             clock_speed,
             vram_speed,
-            temp,
+            temperature,
             power_usage,
             power_cap,
             power_cap_max,
@@ -99,7 +99,7 @@ pub trait NpuImpl {
     fn temperature(&self) -> Result<f64>;
     fn power_usage(&self) -> Result<f64>;
     fn core_frequency(&self) -> Result<f64>;
-    fn vram_frequency(&self) -> Result<f64>;
+    fn memory_frequency(&self) -> Result<f64>;
     fn power_cap(&self) -> Result<f64>;
     fn power_cap_max(&self) -> Result<f64>;
 
@@ -358,10 +358,10 @@ impl Npu {
         }
     }
 
-    pub fn vram_frequency(&self) -> Result<f64> {
+    pub fn memory_frequency(&self) -> Result<f64> {
         match self {
-            Npu::Intel(npu) => npu.vram_frequency(),
-            Npu::Other(npu) => npu.vram_frequency(),
+            Npu::Intel(npu) => npu.memory_frequency(),
+            Npu::Other(npu) => npu.memory_frequency(),
         }
     }
 
